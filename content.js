1b4fd96bce19bfb1ddeecbb87d30c0209e926826
Alexander Kazakov <alexander.kazakov@booking.com>
2020-07-11 02:10:07 +0200

    Refactor code for easy understanding

--- content.js
+++ content.js
@@ -1,53 +1,57 @@
 var isshown=false
 
-var modifyComments = function(){
-    console.log("Notion Layout Extension: Document changed");
-    var targets = document.querySelectorAll(".notion-scroller.vertical > div:nth-child(2)[style='width: 100%; font-size: 14px;']");
-    targets.forEach(function(target){
-        console.log("Found entries");
+// This function is called any time something new loads inside Notion window
+// e.g. you navigate to a new page
+var onPageChange = function(){
+    console.log("Extension: Page changed");
 
-        let abutton = document.createElement("button");
-        abutton.innerHTML = "-";
-        abutton.style.fontSize = "4px";
-        abutton.style.border = "none";
-        abutton.style.width = "100%";
-        abutton.id = "myButton";
-        abutton.onclick = function(){
-            console.log("Notion Layout Extension: Button clicked");
+    // Find the block with properties list
+    var propertiesLists = document.querySelectorAll(".notion-scroller.vertical > div:nth-child(2)[style='width: 100%; font-size: 14px;']");
+
+    propertiesLists.forEach(function(propertiesList){
+        console.log("Found properties list");
+
+        // Set up the toggle button
+        let toggleButton = document.createElement("button");
+        toggleButton.innerHTML = "-";
+        toggleButton.style.fontSize = "4px";
+        toggleButton.style.border = "none";
+        toggleButton.style.width = "100%";
+        toggleButton.id = "toggleButton";
+        toggleButton.onclick = function(){
+            console.log("Extension: Button clicked");
             if (isshown){
-                target.style.height = 0;
-                target.style.display = "none";
+                // Hide properties list section
+                propertiesList.style.height = 0;
+                propertiesList.style.display = "none";
                 isshown = false
             }
             else {
-                target.style.height = null;
-                target.style.display = null;
+                // Show properties list section
+                propertiesList.style.height = null;
+                propertiesList.style.display = null;
                 isshown = true
             }
         }
 
-        if (!(target.id === "A")){
-            console.log("Processing new entry");
-            let parentDiv = target.parentNode
-            var content = target.parentElement.firstChild.firstChild
-            content.appendChild(abutton)
-            console.log("Processing new entry2");
+        // If not already processed this properties list,
+        // add the toggle button and hide the list
+        if (!(propertiesList.id === "already_processed")){
+            console.log("Processing new properties list");
+            var parentBlockForButton = propertiesList.parentElement.firstChild.firstChild
+            parentBlockForButton.appendChild(toggleButton)
 
-            target.style.height = 0;
-            target.style.display = "none";
-            target.id = "A";
+            propertiesList.style.height = 0;
+            propertiesList.style.display = "none";
+            propertiesList.id = "already_processed";
         }
     })
 }
 
-// Select the node that will be observed for mutations
+
+// This calls onPageChange function each time Notion window changes
+// e.g. you navigate to a new Notion page
 const targetNode = document.body;
-
-// Options for the observer (which mutations to observe)
 const config = {attributes: false, childList: true, subtree: true };
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(modifyComments);
-
-// Start observing the target node for configured mutations
+const observer = new MutationObserver(onPageChange);
 observer.observe(targetNode, config);
